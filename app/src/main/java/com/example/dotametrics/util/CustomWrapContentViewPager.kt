b149d79049dd8b7f7e38248ada0173be6e12795b
Robert Mikhailovskiy <kazimir32282@gmail.com>
2023-04-11 21:13:27 +0200

    Wrap custom viewpager in scrollview in match info

---
+++ app/src/main/java/com/example/dotametrics/util/CustomWrapContentViewPager.kt
@@ -0,0 +1,29 @@
+package com.example.dotametrics.util
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.viewpager.widget.ViewPager
+
+// From: https://stackoverflow.com/a/63599814
+class CustomWrapContentViewPager(cont: Context, attr: AttributeSet?) : ViewPager(cont, attr) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        var height = 0
+        for (i in 0 until childCount) {
+            val child: View = getChildAt(i)
+            child.measure(
+                widthMeasureSpec,
+                MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED)
+            )
+            val h: Int = child.measuredHeight
+            if (h > height) height = h
+        }
+        val heightMeasure = MeasureSpec.makeMeasureSpec(
+            height,
+            MeasureSpec.EXACTLY
+        )
+        super.onMeasure(widthMeasureSpec, heightMeasure)
+    }
+}
